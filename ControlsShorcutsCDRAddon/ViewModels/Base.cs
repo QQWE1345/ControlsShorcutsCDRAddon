bf39fe39105d1e5a94c78952b312ed073a6ac005
Reginaldo Santos <bonus630@gmail.com>
2021-03-22 16:14:34 -0300

    Add project files.

---
+++ ControlsShorcutsCDRAddon/ViewModels/Base.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace ControlsShorcutsCDRAddon.ViewModels
+{
+    public class Base : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnNotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
